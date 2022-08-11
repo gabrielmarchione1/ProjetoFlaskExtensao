54db4505df3593c46f162a76892969841e10d788
gabrielmarchione1 <gabrielmarchione12@gmail.com>
2022-08-11 17:15:51 -0300

    passando variaveis produtos estilo dicionario

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+# coding: utf-8
 from flask import Flask, render_template
+
 app = Flask("projeto")
 
 @app.route("/")
 def ola_mundo():
-    return render_template('alo.html'), 200
+    #criar uma variável com o meu nome
+    nome="Gabriel Marchione"
+    produtos = [
+    {"nome": "Ração", "preco": 199.99},
+    {"nome": "Playstation", "preco": 1999.99}]
+
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
 app.run()
