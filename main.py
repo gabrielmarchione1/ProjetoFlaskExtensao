427ec20b1b607a803e1b0d65b244c0c55ed52305
gabrielmarchione1 <gabrielmarchione12@gmail.com>
2022-08-18 10:21:35 -0300

    criando uma nova rota teste

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 app = Flask("projeto")
 
+# rota raiz
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
@@ -10,7 +11,12 @@ def ola_mundo():
     produtos = [
     {"nome": "Ração", "preco": 199.99},
     {"nome": "Playstation", "preco": 1999.99}]
-
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
+# rota teste
+@app.route("/teste")
+@app.route("/teste/<variavel>")
+def funcao_teste(variavel = ""):
+    return "nova rota teste<br>Variável : {}".format(variavel), 200
 
 app.run()
