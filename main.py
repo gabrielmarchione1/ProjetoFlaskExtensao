4b7c63087828465562522e90cd98d090e0d415f1
gabrielmarchione1 <gabrielmarchione12@gmail.com>
2022-08-18 11:09:38 -0300

    rota de formulário e de tratamento do formulário

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("projeto")
 
@@ -13,10 +13,28 @@ def ola_mundo():
     {"nome": "Playstation", "preco": 1999.99}]
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
+
 # rota teste
 @app.route("/teste")
 @app.route("/teste/<variavel>")
 def funcao_teste(variavel = ""):
     return "nova rota teste<br>Variável : {}".format(variavel), 200
+
+
+# rota formulario
+@app.route("/form")
+def form():
+    return render_template("form.html"), 200
+
+
+# rota tratamento do formulario
+@app.route("/form_recebe", methods=["GET", "POST"])
+def form_recebe():
+    nome = ""
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome), 200
+    else:
+        return "Não pode chamar direto no GET", 200
 
 app.run()
